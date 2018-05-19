07ab744ff06c9c8754da27c7069341059d10d63b
ekukura <kukuraemily@gmail.com>
2018-05-19 15:24:24 -0400

    separated launcher from main program

--- src/core/groove_id_program.py
+++ src/core/groove_id_program.py
@@ -4,35 +4,12 @@ Created on May 19, 2018
 @author: emilykukura
 '''
 
-import os
 
 
-def execute_update():
-    pass
-
-
-def launch_main_program():
+def launch():
     
     print("Main program launched")
     x = 5
     y = 6
     print("Performing a simple addition: {arg0} + {arg1} = {res}".format(arg0 = x, arg1 = y, res = x+y))
 
-
-def run():
-    
-    version_file_name = "version_info.txt"
-    project_source_directory = os.getcwd()
-    version_file_path = os.path.join(project_source_directory, version_file_name)
-    print("version file path: {}".format(version_file_path))
-    with open(version_file_path) as version_file:
-        version = version_file.read().strip()
-        print("version: {}".format(version))
-    #here I want to check remote version from github
-       
-    #launch_main_program()
-
-
-if __name__ == '__main__':
-    
-    run()
